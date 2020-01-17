8ef7368e0a597c7155ca9f2df4e237caa9eb1154
Jared Brink <jaredbrink@jared-brink-mac.local>
2020-01-17 13:35:57 -0700

    Implemented get

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -63,4 +63,16 @@ class LinkedList {
       return this;
     }
   }
+  get(index) {
+    if (index < 0 || index > this.length) {
+      return null;
+    }
+    let count = 0;
+    let current = this.head;
+    while (count !== index) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
 }
