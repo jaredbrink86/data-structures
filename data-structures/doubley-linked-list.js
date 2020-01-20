db42ca7eef76b58560776ab598fe189b6c5018bc
Jared Brink <jaredbrink@jared-brink-mac.local>
2020-01-20 09:01:12 -0700

    Implemented pop and shift

--- data-structures/doubley-linked-list.js
+++ data-structures/doubley-linked-list.js
@@ -12,9 +12,47 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
-  push(val) {}
-  pop() {}
-  shift() {}
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (this.length === 0) return undefined;
+    let popped = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = popped.prev;
+      this.tail.next = null;
+      popped.prev = null;
+    }
+    this.length--;
+    return popped;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let shifted = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = shifted.next;
+      this.head.prev = null;
+      shifted.next = null;
+    }
+    this.length--;
+    return shifted;
+  }
   unshift(val) {}
   get(index) {}
   set(index, val) {}
