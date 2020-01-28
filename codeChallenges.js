2dc193dbdf26321ab8a83b6ab71a6d35faa50f60
Jared Brink <jaredbrink86@gmail.com>
2020-01-27 21:17:12 -0700

    Implemented numPrimorial with helper. Will refactor later

--- codeChallenges.js
+++ codeChallenges.js
@@ -21,3 +21,26 @@ function sumTwoSmallestNumbers(numbers) {
   });
   return numbers[0] + numbers[1];
 }
+
+function numPrimorial(n) {
+  let count1 = 1;
+  let count2 = 1;
+  let result = 1;
+  while (count1 <= n) {
+    if (isPrime(count2)) {
+      result = result * count2;
+      count1++;
+    }
+    count2++;
+  }
+  return result;
+}
+
+function isPrime(value) {
+  for (let i = 2; i < value; i++) {
+    if (value % i === 0) {
+      return false;
+    }
+  }
+  return value > 1;
+}
