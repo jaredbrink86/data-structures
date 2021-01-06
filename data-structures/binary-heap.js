150f81876ead9e5d06e0be2fa9f466410c44dac8
Jared Brink <jaredbrink86@gmail.com>
2021-01-05 21:35:00 -0700

    Refactored insert and extractMax

--- data-structures/binary-heap.js
+++ data-structures/binary-heap.js
@@ -4,55 +4,47 @@ class MaxBinaryHeap {
     }
     insert(val) {
         this.values.push(val);
-        let valIndex = this.values.length - 1;
-        let parentIndex = Math.floor((valIndex - 1) / 2);
-        let temp;
-        let tempIndex;
-        while(this.values[parentIndex] < this.values[valIndex]){
-            temp = this.values[valIndex];
-            this.values[valIndex] = this.values[parentIndex];
-            this.values[parentIndex] = temp;
-            tempIndex = valIndex;
-            valIndex = parentIndex;
-            parentIndex = valIndex;
+        this.bubbleUp()
+    }
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        const element = this.values[idx]
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx]
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
         }
-        return this.values
     }
     extractMax() {
        const max = this.values[0];
        const end = this.values.pop();
-       if(this.values.length > 0) {
-           this.values[0] = end;
-           this.sinkDown();
-        }
-        return max
+       this.values[0] = end;
+       this.sinkDown();
+       return max;
     }
     sinkDown() {
         let idx = 0;
         let element = this.values[0];
-        let leftChild, rightChild
-        while(true){
-            let leftChildIdx = 2 * idx + 1;
-            let rightChildIdx = 2 * idx + 2;
+        let leftChild, rightChild, leftChildIdx, rightChildIdx
+        while(true) {
+            leftChildIdx = 2 * idx + 1
+            rightChildIdx = 2 * idx + 2
             leftChild = this.values[leftChildIdx]
             rightChild = this.values[rightChildIdx]
-            if(leftChild > element && leftChild > rightChild) {
-                this.values[idx] = leftChild;
-                this.values[leftChildIdx] = element;
-                idx = leftChildIdx
-                element = this.values[leftChildIndx]
-            }
-            if(rightChild > element && rightChild > leftChild) {
+            if(rightChild > element && rightChild > leftChild && rightChild !== undefined) {
                 this.values[idx] = rightChild
-                this.values[rightChild] = element;
+                this.values[rightChildIdx] = element
                 idx = rightChildIdx
-                element = this.values[rightChildIdx]
-            }
-            if((rightChild < element && leftChild < element) || (leftChild === undefined && rightChild === undefined)) break;
-        }
+                element = this.values[idx]
+            } else if(leftChild > element && leftChild > rightChild && leftChild !== undefined) {
+                this.values[idx] = leftChild
+                this.values[leftChildIdx] = element
+                idx = leftChildIdx
+                element = this.values[idx]
+            } else break;
+        }   
     }
 }
-
-[4,89,76,65,54,33,11,23]
-
-100
